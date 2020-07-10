688a08a8e957f17be3319f16ace2821577c574e9
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2020-07-10 15:32:56 +0000

    Stub out SOAP call

--- bin/index.js
+++ bin/index.js
@@ -1,20 +1,61 @@
 const soapRequest = require('easy-soap-request');
 const fs = require('fs');
+const { js2xml, xml2js } = require("xml-js");
 
-// example data
-const url = 'https://graphical.weather.gov/xml/SOAP_server/ndfdXMLserver.php';
-const sampleHeaders = {
-  'user-agent': 'sampleTest',
-  'Content-Type': 'text/xml;charset=UTF-8',
-  'soapAction': 'https://graphical.weather.gov/xml/DWMLgen/wsdl/ndfdXML.wsdl#LatLonListZipCode',
+const user = "cv3hueysoap";
+const xmlData = "./data/jld-5053.xml"
+
+require("dotenv").config({
+  path: `.env.${user}`
+});
+
+const _declaration = {
+  _attributes: {
+    version: "1.0",
+    encoding: "utf-8"
+  }
 };
-const xml = fs.readFileSync('./test/zip-code-envelope.xml', 'utf-8');
 
-// usage of module
+const request = {
+  authenticate: {
+    user: { _text: process.env.CV3_USER },
+    pass: { _text: process.env.CV3_PASS },
+    serviceID: { _text: process.env.CV3_SERVICE_ID }
+  }
+};
+
+const CV3DataRaw = fs.readFileSync(xmlData, 'utf-8');
+const { CV3Data: { _attributes, products } } = xml2js(CV3DataRaw, { compact: true });
+
+const CV3DataXML = js2xml(
+  {
+    _declaration,
+    CV3Data: {
+      _attributes,
+      request,
+      products
+    }
+  }, {
+    compact: true,
+  }
+);
+
+const CV3DataBase64 = Buffer.from(CV3DataXML, "ascii").toString("base64");
+
+const xml = '<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://www.w3.org/2001/12/soap-envelope" SOAP-ENV:encodingStyle="http://www.w3.org/2001/12/soap-encoding"><SOAP-ENV:Body><m:CV3Data xmlns:m="http://soapinterop.org/" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><data xsi:type="xsd:string">' + CV3DataBase64 + '</data></m:CV3Data></SOAP-ENV:Body></SOAP-ENV:Envelope>';
+
+const url = 'https://service.commercev3.com/';
+const requestHeaders = {
+  'Accept': 'text/xml',
+  'Content-Type': 'text/xml',
+  'soapAction': 'https://service.commercev3.com/index.php/CV3Data',
+  'user-agent': 'Huey - node',
+};
+
 (async () => {
-  const { response } = await soapRequest({ url: url, headers: sampleHeaders, xml: xml, timeout: 1000 }); // Optional timeout parameter(milliseconds)
+  const { response } = await soapRequest({ url, headers: requestHeaders, xml, timeout: 1000 });
   const { headers, body, statusCode } = response;
   console.log(headers);
-  console.log(body);
+  console.log(Buffer.from(xml2js(body, { compact: true })['SOAP-ENV:Envelope']['SOAP-ENV:Body']['ns1:CV3DataResponse']['return']['_text'], "base64").toString('ascii'));
   console.log(statusCode);
 })();
